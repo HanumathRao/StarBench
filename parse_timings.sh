4cfac4c4bc8ab24fc429a7aafefd8c0a3f5e2c4a
Hanumath Maduri <hanu.ncr@gmail.com>
2024-06-02 09:22:40 -0700

    Fixing the parse_timings.sh to use starbench instead of ssb

--- parse_timings.sh
+++ parse_timings.sh
@@ -12,14 +12,14 @@ if [ ! -d "$1" ]; then
     exit 1
 fi
 
-printf "%-10s \t %-10s \t %-10s\n" "Query" "tpch_timing(ms)" "ssb_timing(ms)"
+printf "%-10s \t %-10s \t %-10s\n" "Query" "tpch_timing(ms)" "starbench_timing(ms)"
 printf "%s\n" "----------------|----------------|-------------------"
 for i in {01..22}; do
-    if [ -f "$1/tpch/OUT/Q$i.timing" ] && [ -f "$1/ssb/OUT/Q$i.timing" ]; then
+    if [ -f "$1/tpch/OUT/Q$i.timing" ] && [ -f "$1/starbench/OUT/Q$i.timing" ]; then
 	tpch_timing=`cat $1/tpch/OUT/Q$i.timing`
-	ssb2_timing=`cat $1/ssb/OUT/Q$i.timing`
+	starbench_timing=`cat $1/starbench/OUT/Q$i.timing`
 	tpch_timing_rounded=`echo $tpch_timing | awk '{print int($1*1000)}'`
-	ssb2_timing_rounded=`echo $ssb2_timing | awk '{print int($1*1000)}'`
-        printf "Q%-14s \t %-12s \t %-10s\n" "$i" "$tpch_timing_rounded" "$ssb2_timing_rounded"
+	starbench_timing_rounded=`echo $starbench_timing | awk '{print int($1*1000)}'`
+        printf "Q%-14s \t %-12s \t %-10s\n" "$i" "$tpch_timing_rounded" "$starbench_timing_rounded"
     fi
 done
